84692a9bd426deebc744b7fe61136932ef58495e
bestbags-official <mezzika33@gmail.com>
2026-04-25 01:35:39 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'kosti-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js'
+];
+
+self.addEventListener('install', async el => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', el => {
+  el.respondWith(
+    caches.match(el.request).then(res => res || fetch(el.request))
+  );
+});
